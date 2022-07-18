9bef939b9181c38f143d6f447a319ffdce054c38
Anna Lee <annakatherinelee@me.com>
2022-07-18 16:35:50 -0400

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,9 @@
+// from data.js
+const tableData = data;
+// get table references
+var tbody = d3.select("tbody");
 function buildTable(data) {
+  
     // First, clear out any existing data
     tbody.html("");
   
